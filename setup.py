936d91bb06f482bde358be7790d0fd502f55aec7
Michael Barney, Jr <mbarneyme@gmail.com>
2021-08-20 17:02:10 -0400

    fixes region in post_add_arguments

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='awsume-console-plugin',
-    version='1.2.1',
+    version='1.2.2',
     entry_points={
         'awsume': [
             'console = console'
